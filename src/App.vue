9ae92dbec35cdf03d3885c8bac20cf1bfde3acea
Suujp <inflames1012@gmail.com>
2022-12-19 00:00:36 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }} : {{ uuid }}</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { v4 as uuidv4 } from 'uuid'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data: function() {
+    return {
+      message: 'uuid',
+      uuid: uuidv4(),
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
